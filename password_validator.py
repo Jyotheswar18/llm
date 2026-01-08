c860d924c8268e36479294811de5d911734ea14e
Jyotheswar18 <Chinnapapireddygari2023@vitstudent.ac.in>
2026-01-08 21:27:20 +0530

    Initial commit

---
+++ password_validator.py
@@ -0,0 +1,18 @@
+import re
+
+def check_password(password):
+    if len(password) < 6 or len(password) > 12:
+        return False
+    if not re.search("[a-z]", password):
+        return False
+    if not re.search("[0-9]", password):
+        return False
+    if not re.search("[A-Z]", password):
+        return False
+    if not re.search("[$#@]", password):
+        return False
+    return True
+
+passwords = input().split(',')
+valid_passwords = [p for p in passwords if check_password(p)]
+print(",".join(valid_passwords))
